4d16a717bc7d61fa5dc081ea87f26c911d7498fe
Mobin <107855827+MB18Front@users.noreply.github.com>
2022-08-31 20:25:48 +0430

    Update methods.js

--- methods.js
+++ methods.js
@@ -2,13 +2,6 @@
 const len = (data) => {
   return data.length;
 };
-const $ = (data) => {
-  let element = document.querySelectorAll(data);
-  if (len(element) === 1) {
-    return element[0];
-  };
-  return element;
-};
 //easy Math
 const floor = (number) => {
   return Math.floor(number);
@@ -19,5 +12,3 @@ const ceil = (number) => {
 const round = (number) => {
   Math.round(number);
 };
-
-export { len, $, floor, ceil, round };
